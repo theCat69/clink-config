ece13aaea7dfc4221d98b05576f212775611efb1
Felix Vadcard <vadcard.felix@gmail.com>
2023-01-11 21:40:21 +0100

    optimizing git repository detection to speed navigation outside git directory

--- init.lua
+++ init.lua
@@ -2,23 +2,28 @@ local cyan   = "\x1b[36m"
 local green  = "\x1b[92m"
 local normal = "\x1b[m"
 
--- prompt filter to add repo url in green
-local git_repo_prompt = clink.promptfilter(60)
-function git_repo_prompt:filter(prompt)
-  local line = io.popen("git config --get remote.origin.url"):read("*a")
-  local repo = line:match("(.+)\n")
-  if repo then
-    return prompt .. "\n" .. cyan .. "[" .. repo .. "]" .. normal
-  end
-end
+local is_git_repo = io.popen("git rev-parse --is-inside-work-tree 2>&1"):read("*a"):match("true");
 
--- prompt filter to add current branch in cyan
-local git_branch_prompt = clink.promptfilter(65)
-function git_branch_prompt:filter(prompt)
-  local line = io.popen("git branch --show-current 2>nul"):read("*a")
-  local branch = line:match("(.+)\n")
-  if branch then
-    return prompt .. " " .. green .. "(" .. branch .. ")" .. normal
+-- git repository specific stuff
+if is_git_repo then
+  -- prompt filter to add repo url in green
+  local git_repo_prompt = clink.promptfilter(60)
+  function git_repo_prompt:filter(prompt)
+    local line = io.popen("git config --get remote.origin.url"):read("*a")
+    local repo = line:match("(.+)\n")
+    if repo then
+      return prompt .. "\n" .. cyan .. "[" .. repo .. "]" .. normal
+    end
+  end
+
+  -- prompt filter to add current branch in cyan
+  local git_branch_prompt = clink.promptfilter(65)
+  function git_branch_prompt:filter(prompt)
+    local line = io.popen("git branch --show-current 2>nul"):read("*a")
+    local branch = line:match("(.+)\n")
+    if branch then
+      return prompt .. " " .. green .. "(" .. branch .. ")" .. normal
+    end
   end
 end
 
